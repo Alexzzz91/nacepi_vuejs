80e3d126dad2f3076a39c65db886a223b47592fe
alk <belalex.9132788@gmail.com>
2017-07-12 14:35:35 +0600

    reserve page almost complite

--- client/components/Reserve/Reserve.vue
+++ client/components/Reserve/Reserve.vue
@@ -7,8 +7,92 @@
 	    <div v-if="error" class="error">
 	      {{ error }}
 	    </div>
-
-	    <div v-if="reservs.length > 0" class="content">
+	    <div class="week">
+	    	<template v-for="day in days">
+	    		<div class="row">
+	    			<div class="day">
+						<p class="day-name">{{ day.day }}</p>
+						<p class="day-number">{{ day.number }}</p>
+						<p class="day-mounth">{{ day.mounth }}</p>
+	    			</div>
+					<div class="reserve-time">
+						<div class="reserve-time-item">
+							<div class="time">
+								12:00
+							</div>
+							<div class="price">
+								2000 р.
+							</div>
+						</div>
+						<div class="reserve-time-item">
+							<div class="time">
+								13:30
+							</div>
+							<div class="price">
+								2000 р.
+							</div>
+						</div>
+						<div class="reserve-time-item">
+							<div class="time">
+								15:00
+							</div>
+							<div class="price">
+								2000 р.
+							</div>
+						</div>
+						<div class="reserve-time-item">
+							<div class="time">
+								16:30
+							</div>
+							<div class="price">
+								2000 р.
+							</div>
+						</div>
+						<div class="reserve-time-item">
+							<div class="time">
+								18:00
+							</div>
+							<div class="price">
+								2000 р.
+							</div>
+						</div>
+						<div class="reserve-time-item">
+							<div class="time">
+								19:30
+							</div>
+							<div class="price">
+								2000 р.
+							</div>
+						</div>
+						<div class="reserve-time-item">
+							<div class="time">
+								21:00
+							</div>
+							<div class="price">
+								2000 р.
+							</div>
+						</div>
+						<div class="reserve-time-item">
+							<div class="time">
+								23:00
+							</div>
+							<div class="price">
+								2300 р.
+							</div>
+						</div>
+						<div class="reserve-time-item">
+							<div class="time">
+								после 24:00
+							</div>
+							<div class="price">
+								2300 р.
+							</div>
+						</div>
+					</div>	
+	    		</div>
+	    	</template>
+	    </div>
+	    <div v-if="reservs.length > 0" >
 	    	<template v-for="reserv in reservs">
 				<h2>{{ reserv.name }}</h2>
 				<p>{{ reserv.date }}</p>
@@ -17,22 +101,38 @@
 	</div>
 </template>
 
-
 <script>
 import axios from 'axios';
+import * as moment from 'moment';
+moment.locale('ru');
 export default {
 	name: 'ReservComponent',
 	data () {
 		return {
 			loading: false,
 			reservs: [],
-			error: null
+			error: null, 
+			days : []
 		}
 	},
 	created () {
 		// fetch the data when the view is created and the data is
 		// already being observed
-		this.fetchData()
+		this.fetchData();
+		const today ={
+			'day':moment().format('dddd'),
+			'number': moment().format('D'),
+			'mounth' : moment().format('MMMM') 
+		}
+		this.days.push(today);
+		for (var i = 1; i <= 7; i++) {
+			let day = {
+				'day':moment().add(i, 'days').format('dddd'),
+				'number': moment().add(i, 'days').format('D'),
+				'mounth' : moment().add(i, 'days').format('MMMM') 
+			};
+			this.days.push(day); 
+		}
 	},
 	watch: {
 		// call again the method if the route changes
@@ -42,7 +142,7 @@ export default {
 		fetchData () {
 			this.error = null
 			this.loading = true
-			axios.get(`http://localhost:9001/na_cepi/reserv`)
+			axios.get(`http://109.234.37.128:9001/na_cepi/reserv`)
 			.then(({data}) => {
 				this.loading = false
 				// JSON responses are automatically parsed.
--- client/views/Reserv/Reserv.vue
+++ client/views/Reserv/Reserv.vue
@@ -1,6 +1,36 @@
 <template>
   <div class="na-cepi__reserv">
-    <img src="/RJuN9U3yoaI.jpg" class="na-cepi__reserv-background">
+    <div class="na-cepi__reserv-top">
+      <h1 class="na-cepi__reserv-top-label">Хоррор-квест "Вуду. История Киры"</h1>
+      <div class="na-cepi__reserv-top-container">
+        <img src="/RJuN9U3yoaI.jpg" alt="">
+        <div class="na-cepi__reserv-top-container__limitation"> 16+ </div>
+        <div class="na-cepi__reserv-top-container-bottom">
+          <div class="na-cepi__reserv-top-container__time"> 60 минут </div>
+          <div class="na-cepi__reserv-top-container__team-count"> 2-5 человек </div>
+        </div>
+      </div>
+    </div>
+    <div class="na-cepi__reserv-about">
+      <div class="na-cepi__reserv-about-container">
+        <p>Атмосферный хоррор-квест. Маленькая девочка Кира не заставит вас скучать на протяжении всего времени вашего прибывания в жутком подвале ее милой матери. </p>
+
+        <p>Количество участников от 2 до 5 человек</p>
+
+        <p>Стоимость для команды до 11 вечера 2000 руб.после 2300 руб.</p>
+
+        <p>В будни с пн. по пт. предусмотрены скидки.</p>
+
+        <p>Студентам и школьникам скидка 20% ежедневно.</p>
+
+        <p>Пенсионерам скидка 70%, если придут конечно.</p>
+
+        <p>В день рождения действует скидка в 30%, а так же всю неделю до дня рождения и после.</p>
+
+        <p>В ночное время с 23:00 и до 7:00 скидки не действительны.</p>
+      </div>
+    </div>
+    <img src="/6co-eHWzq7k.jpg" class="na-cepi__reserv-background">
     <div class="na-cepi__reserv-container">
       <reserv-component/>
     </div>
